dd16675e8f6e58e798c7e9ce20506aeefa7eefce
Colin Hanley <colinhanley1836@gmail.com>
2026-01-12 14:41:35 -0600

    added favicon

--- bracket.py
+++ bracket.py
@@ -237,6 +237,7 @@ def generate_html(field_df, l4i, f4o, n4o):
     <!DOCTYPE html>
     <html lang="en">
     <head>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <title>Bracketology | CHan Analytics</title>
