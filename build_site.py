8ca0a222b37ffc06713f387dba1e24f01d9fa148
Colin Hanley <colinhanley1836@gmail.com>
2026-01-14 22:01:42 -0600

    added SLS subrank

--- build_site.py
+++ build_site.py
@@ -287,7 +287,7 @@ def generate_index(df, df_conf):
     df['Rank_AdjT'] = df['AdjT'].rank(ascending=False, method='min').astype(int)
     df['Rank_Cons'] = df['Consistency'].rank(ascending=False, method='min').astype(int)
     df['Rank_WAB'] = df['WAB'].rank(ascending=False, method='min').astype(int)
-    
+    df['Rank_SLS'] = df['Selection_Score'].rank(ascending=False, method='min').astype(int)
     if df_conf is not None:
         df = df.merge(df_conf, on='Team', how='left')
         df['Conference'] = df['Conference'].fillna('Unknown')
@@ -299,7 +299,7 @@ def generate_index(df, df_conf):
     df = df.loc[:, ~df.columns.duplicated()]
 
     cols = ['Rank', 'Team', 'Conference', 'Link', 'AdjEM', 'Pure_AdjEM', 'WAB', 'Consistency', 
-            'Selection_Score', 'AdjO', 'AdjD', 'AdjT', 'Rank_AdjO', 'Rank_AdjD', 'Rank_AdjT', 'Rank_Cons', 'Rank_WAB']
+            'Selection_Score', 'AdjO', 'AdjD', 'AdjT', 'Rank_AdjO', 'Rank_AdjD', 'Rank_AdjT', 'Rank_Cons', 'Rank_WAB', 'Rank_SLS']
     cols = [c for c in cols if c in df.columns]
     rankings_json = df[cols].to_json(orient='records')
 
